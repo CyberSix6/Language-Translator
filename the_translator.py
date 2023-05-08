98bd66db15f44337548ac22cb4c808b0edc22279
stephn nm <braveblackguy@gmail.com>
2023-05-08 18:57:25 -0500

    Linted

--- the_translator.py
+++ the_translator.py
@@ -1,13 +1,18 @@
 """This module contains functions for translating text using Watson Language Translator APIs."""
-
+import re
 from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator 
-
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Setup service & API access
 URL_LT = 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/40324722-7654-4cf0-9f2c-d80386993c61'
-APIKEY_LT = 'u-T7aWU9qQ-x_Xq2BSuhvNaVUujsQ3V3JYkreIxzJStL' 
-VERSION_LT = '2023-05-01'
+APIKEY_LT ='u-T7aWU9qQ-x_Xq2BSuhvNaVUujsQ3V3JYkreIxzJStL'
+VERSION_LT ='2023-05-01'
+
+def remove_whitespace(text):
+    """Remove all the whitespaces and newlines from the text using the regex."""
+    return re.sub(r'\s+', '', text)
+
+
 
 # Create language translator object
 authenticator = IAMAuthenticator(APIKEY_LT)
