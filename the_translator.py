2f7d4973ce2f67c153b8e90d3385ac99705f021f
stephn nm <braveblackguy@gmail.com>
2023-05-08 19:09:32 -0500

    Final draft. Lint score 10/10

--- the_translator.py
+++ the_translator.py
@@ -4,7 +4,8 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Setup service & API access
-URL_LT = 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/40324722-7654-4cf0-9f2c-d80386993c61'
+URL_LT = ('https://api.us-south.language-translator.watson.cloud.ibm.com/'
+          'instances/40324722-7654-4cf0-9f2c-d80386993c61')
 APIKEY_LT ='u-T7aWU9qQ-x_Xq2BSuhvNaVUujsQ3V3JYkreIxzJStL'
 VERSION_LT ='2023-05-01'
 
